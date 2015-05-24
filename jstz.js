5fac65626f4ae6b175d8bb353702d9dc6fc2b824
renatus <pub@renat.biz>
2015-05-25 01:38:06 +0300

    Modify Moscow timezone

--- jstz.js
+++ jstz.js
@@ -451,33 +451,32 @@ jstz.olson.timezones = {
     '120,1': 'Asia/Beirut',
     '120,0': 'Africa/Johannesburg',
     '180,0': 'Asia/Baghdad',
-    '180,0': 'Europe/Moscow',
     '210,1': 'Asia/Tehran',
     '240,0': 'Asia/Dubai',
     '240,1': 'Asia/Baku',
     '270,0': 'Asia/Kabul',
-    '300,0': 'Asia/Yekaterinburg',
+    '300,1': 'Asia/Yekaterinburg',
     '300,0': 'Asia/Karachi',
     '330,0': 'Asia/Kolkata',
     '345,0': 'Asia/Kathmandu',
     '360,0': 'Asia/Dhaka',
-    '360,0': 'Asia/Omsk',
+    '360,1': 'Asia/Omsk',
     '390,0': 'Asia/Rangoon',
-    '420,0': 'Asia/Krasnoyarsk',
+    '420,1': 'Asia/Krasnoyarsk',
     '420,0': 'Asia/Jakarta',
     '480,0': 'Asia/Shanghai',
-    '480,0': 'Asia/Irkutsk',
+    '480,1': 'Asia/Irkutsk',
     '525,0': 'Australia/Eucla',
     '525,1,s': 'Australia/Eucla',
-    '540,0': 'Asia/Yakutsk',
+    '540,1': 'Asia/Yakutsk',
     '540,0': 'Asia/Tokyo',
     '570,0': 'Australia/Darwin',
     '570,1,s': 'Australia/Adelaide',
     '600,0': 'Australia/Brisbane',
-    '600,0': 'Asia/Vladivostok',
+    '600,1': 'Asia/Vladivostok',
     '600,1,s': 'Australia/Sydney',
     '630,1,s': 'Australia/Lord_Howe',
-    '720,0': 'Asia/Kamchatka',
+    '660,1': 'Asia/Kamchatka',
     '660,0': 'Pacific/Noumea',
     '690,0': 'Pacific/Norfolk',
     '720,1,s': 'Pacific/Auckland',
@@ -1305,7 +1304,8 @@ jstz.olson.dst_rules = {
             ]
         }
     ]
-};    if (typeof exports !== 'undefined') {
+};
+    if (typeof exports !== 'undefined') {
         exports.jstz = jstz;
     } else {
         root.jstz = jstz;
